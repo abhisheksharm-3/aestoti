1432b459fdeba5ccc65afd0a2e4b7beb2c2d053f
Abhishek Sharma <abhitiku2003@gmail.com>
2024-08-19 19:10:56 +0530

    minor build issues

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,7 +1,11 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
+import type { Models } from "node-appwrite";
 declare global {
 	namespace App {
+		interface Locals {
+			user: Models.User<Models.Preferences> | undefined;
+		  }
 		// interface Error {}
 		// interface Locals {}
 		// interface PageData {}
